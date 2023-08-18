8375830051a1e136f1daa8065df1a1979e976d9a
Cleo <88383789+cleo-d@users.noreply.github.com>
2023-08-18 12:29:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0c728c250fb0ecec2731ab8759d6826a&page=1'
+const IMAGE_Path = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=0c728c250fb0ecec2731ab8759d6826a&query="'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+//Get initial movies
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+            <img src="${IMAGE_Path + poster_path}"
+                alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>${title}</h3>
+                ${overview}
+            </div>
+        `;
+
+        // Add click event listener to open a new tab with movie details
+        movieEl.addEventListener('click', () => {
+            openMovieDetailsWindow(movie);
+        });
+
+        main.appendChild(movieEl);
+    });
+}
+
+
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5 ) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
+
